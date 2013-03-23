198f1ea1221bab46e03046a374bc6e5712091ed1
Dogy Vogey <vogey@dogyvogey.com>
2013-03-24 00:25:21 +0200

    Create contact_submit.php

---
+++ contact_submit.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* This program is owened and written by:
+* Author: Vaugen Wakeling
+* Company: Astro Web
+* Address: www.astroweb.co.za
+* Version: 1.0
+* Don't remove this comment!!!
+*/
+
+//Require contact class from class folder
+require 'contact.class.php';
+
+//Start ContactSubmit class
+$email = new ContactSubmit();
+
+//Collect Pramaters
+$email->from = $_POST['email'];
+$email->sendTo = "vogey@dogyvogey.com";
+$email->name = $_POST['name'];
+$email->subject = $_POST['subject'];
+$email->message = "
+From: ". $_POST['email'] . "
+Name: " . $_POST['name'] . "
+Subject: " . $_POST['subject'] . "
+Message: " . $_POST['message'] . "
+
+<p>Powered By: Dogy Vogey PHP OOP Email <a href='https://github.com/Dogyvogey'>View Projet</a></p>";
+
+//SendMail
+$email->sendMail();
+
+//Redirect
+$email->redirect("form.php?mess=1");
+
+?>
